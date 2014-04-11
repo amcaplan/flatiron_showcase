6d7045cec5f162cc8a86ab3d6610b0689a7e1b4f
amcaplan <ariel.caplan@mail.yu.edu>
2014-04-11 14:01:16 -0400

    Add utility methods to project and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   has_many :user_projects, dependent: :destroy
   has_many :projects, through: :user_projects
 
+  def self.create_from_hash(user_info)
+    User.create(github_login: user_info.login,
+      name: user_info.login,
+      avatar_url: user_info.avatar_url)
+  end
+
   def github_auth
     @github_auth = self.authorizations.where.not(github_uid: nil).first
   end
