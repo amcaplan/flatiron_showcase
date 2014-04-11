98d75aaa3d2cc7ef33080e282675442471ebfc01
amcaplan <ariel.caplan@mail.yu.edu>
2014-04-11 11:05:16 -0400

    Add seeds for AppType

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+['developer tools', 'CMS', 'social media', 'the internet of things', 'dashboards', 'food', 'data'].each do |name|
+  AppType.create(name: name)
+end
