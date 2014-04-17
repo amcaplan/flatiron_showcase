81a49f2cad3fe6bb2770a80ac2971477f5f4f001
amcaplan <ariel.caplan@mail.yu.edu>
2014-04-17 18:42:20 -0400

    Fix up look and functionality of showcase page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def new_tab_link(*args)
-    link_to(*args, target: '_blank')
+  def new_tab_link(*args, &block)
+    link_to(*args, target: '_blank', &block)
   end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,5 @@
 module StaticPagesHelper
+  def split_to_length(str, length)
+    str.scan(/\S.{1,#{length}}(?!\S)/)
+  end
 end
