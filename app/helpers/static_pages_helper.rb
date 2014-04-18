70869165556f6f0739389d157f74a92722581e59
amcaplan <ariel.caplan@mail.yu.edu>
2014-04-18 15:03:58 -0400

    Fix up display of project's students in the showcase

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,4 +2,15 @@ module StaticPagesHelper
   def split_to_length(str, length)
     str.scan(/\S.{1,#{length}}(?!\S)/)
   end
+
+  def image_size(project)
+    return @image_size if @last_project == project
+    @last_project = project
+    tens_of_projects = project.users.size / 10
+    if tens_of_projects == 0
+      @image_size = 100
+    else
+      @image_size = 100 / (Math.sqrt(2) * Math.log(tens_of_projects, 2).to_i)
+    end
+  end
 end
