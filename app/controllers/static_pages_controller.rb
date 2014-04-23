8d57bb7cc1838c08c0b58e9f6267de4b48287f7a
amcaplan <ariel.caplan@mail.yu.edu>
2014-04-23 13:21:28 -0400

    Stop fetching projects for homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
-    @projects = Project.where.not(brief_description: nil).last(7).shuffle.first(3)
-    # TODO Remove before launching for good
-    if @projects.length < 3
-      @projects = [@projects[0], @projects[0], @projects[0]]
-    end
+    # MAY BE USED LATER
+    # @projects = Project.where.not(brief_description: nil).last(10).shuffle.first(3)
   end
 
   def showcase
