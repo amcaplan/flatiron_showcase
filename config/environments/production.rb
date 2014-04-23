99d69cdbdb923de388697e15488ffbd0a4b9ead8
amcaplan <ariel.caplan@mail.yu.edu>
2014-04-22 22:46:31 -0400

    Add direct reference to revolution plugin folder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,6 @@ Rails.application.configure do
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
   config.assets.precompile += %w(.svg .eot .woff .ttf)
   config.assets.paths << Rails.root.join('vendor', 'assets', 'stylesheets', 'font')
-  config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts')
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts', 'Flatpoint-js', 'rs-plugin', 'pluginsources')
   config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
 end
