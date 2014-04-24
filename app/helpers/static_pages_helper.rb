4be0c9176779a5b4405ac673c4398d26e18f8359
amcaplan <ariel.caplan@mail.yu.edu>
2014-04-24 19:58:59 -0400

    Fix up showcase view with word-wrap and ellipses

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,19 @@
 module StaticPagesHelper
-  def split_to_length(str, length)
-    str.scan(/\S.{1,#{length}}(?!\S)/)
+  def add_newlines(str)
+    safe_join(str.split(/\r\n\r/), tag(:br))
+  end
+
+  def split_string(str, length)
+    lines = str.scan(/\S.{1,#{length}}(?!\S)/).map {|line|
+      add_newlines(line)
+    }
+    if lines[9]
+      until lines[9].end_with?("...")
+        lines[9] << "."
+      end
+      lines = lines[0, 10]
+    end
+    safe_join lines, tag(:br)
   end
 
   def image_size(project)
